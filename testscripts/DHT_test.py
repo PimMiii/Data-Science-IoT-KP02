8dd52ef0f9754404e927eebec777b654d082be16
PimMiii <pim.milt@gmail.com>
2022-01-28 15:05:32 +0100

    button_events.py in acceptable state. DHT_test frequency set to 15 seconds.

--- testscripts/DHT_test.py
+++ testscripts/DHT_test.py
@@ -10,6 +10,7 @@ while True:
     if humidity is not None and temperature is not None:
         print(
             "Temp={0:0.1f}C  Humidity={1:0.1f}%".format(temperature, humidity))
+        print("raw data: Temp; " + temperature +"C Humidity; " + humidity)
     else:
         print("Sensor failure. Check wiring.")
     time.sleep(15)
