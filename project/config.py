2406481c383bc5aa42d338570c70b4c53e89778f
PimMiii <pim.milt@gmail.com>
2022-01-29 13:49:44 +0100

    Updated config.py

--- project/config.py
+++ project/config.py
@@ -1,8 +1,8 @@
-'''
-configure your thingspeak information here
-'''
+# configure your thingspeak information here
 
-
-writeAPIkey = "YOUR-CHANNEL-WRITEAPIKEY" # Replace YOUR-CHANNEL-WRITEAPIKEY with your channel write API key
-channelID = "YOUR-CHANNELID" # Replace YOUR-CHANNELID with your channel ID
-url = "https://api.thingspeak.com/channels/"+channelID+"/bulk_update.json" # ThingSpeak server settings
+# Replace YOUR-CHANNEL-WRITEAPIKEY with your channel write API key
+writeAPIkey = "YOUR-CHANNEL-WRITEAPIKEY"
+# Replace YOUR-CHANNELID with your channel ID
+channelID = "YOUR-CHANNELID"
+# ThingSpeak server settings
+url = "https://api.thingspeak.com/update?api_key=" + writeAPIkey
