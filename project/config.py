d176716d2d7c72f4e58054ab18c395a3c74ceba4
PimMiii <pim.milt@gmail.com>
2022-01-30 15:02:09 +0100

    added bot_prefix to config.py

--- project/config.py
+++ project/config.py
@@ -8,6 +8,8 @@ channelID = "YOUR-CHANNELID"
 url = "https://api.thingspeak.com/update?api_key=" + writeAPIkey
 
 
-
-# If you want to use the Discord Bot as well enter your bot's Token here
-discord_bot_token = "YOUR-TOKEN-HERE"
+# configure your Discord bot information here
+# If you want to use the Discord Bot as well enter your DISCORD-BOT-TOKEN here
+discord_bot_token = "DISCORD-BOT-TOKEN"
+# the prefix the bot will look for. default = !
+discord_bot_prefix = "!"
